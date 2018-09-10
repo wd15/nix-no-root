1a8942ef1e058e8146194ef56c20596559329340
Jeffrey David Johnson <jefdaj@berkeley.edu>
2018-09-09 20:21:10 -0700

    wip

--- nix-no-root.sh
+++ nix-no-root.sh
@@ -14,6 +14,7 @@ export PATH=$BOOTSTRAP_DIR/bin:$PATH
 export PKG_CONFIG_PATH=$BOOTSTRAP_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 export TMPDIR=$NIX_DIR/tmp
 export LD_LIBRARY_PATH=$BOOTSTRAP_DIR/lib:$LD_LIBRARY_PATH
+# export LDFLAGS="-L$BOOTSTRAP_DIR/lib $LDFLAGS -static"
 
 mkdir -p $BOOTSTRAP_DIR || (echo "failed to create $BOOTSTRAP_DIR" && exit 1)
 mkdir -p $TMPDIR   || (echo "failed to create $TMPDIR"   && exit 1)
@@ -21,8 +22,8 @@ mkdir -p $TMPDIR   || (echo "failed to create $TMPDIR"   && exit 1)
 build_libbrotli() {
   # TODO download a specific version?
   cd $BOOTSTRAP_DIR
-  if [ ! -a lib/libbrolidec.so ]; then
-    git clone http://github.com/bagder/libbrotli
+  if [[ ! -a lib/libbrotlidec.a ]]; then
+    [[ -d libbrotli ]] || git clone http://github.com/bagder/libbrotli
     cd libbrotli
     ./autogen.sh
     ./configure --prefix=$BOOTSTRAP_DIR
@@ -34,7 +35,7 @@ build_libbrotli() {
 
 build_libseccomp() {
   cd $BOOTSTRAP_DIR
-  if [ ! -a lib/libseccomp.so ]; then
+  if [[ ! -e lib/libseccomp.so ]]; then
     baseurl="https://github.com/seccomp/libseccomp/releases/download"
     wget "${baseurl}/v${LIBSECCOMP_VERSION}/libseccomp-${LIBSECCOMP_VERSION}.tar.gz"
     tar xvzf libseccomp-${LIBSECCOMP_VERSION}.tar.gz
@@ -51,17 +52,18 @@ build_nix() {
   build_libseccomp
   cd $BOOTSTRAP_DIR
   if [ ! -e $BOOTSTRAP_DIR/bin/nix-env ] ; then
-    if [ ! -e nix-${NIX_VERSION}.tar.bz2 ] ; then
+    if [ ! -d nix-${NIX_VERSION} ] ; then
       wget http://nixos.org/releases/nix/nix-${NIX_VERSION}/nix-${NIX_VERSION}.tar.bz2
+      bzip2 -d nix-${NIX_VERSION}.tar.bz2
+      tar xvf nix-${NIX_VERSION}.tar
     fi
-    bzip2 -d nix-*bz2
-    tar xvf nix-*.tar
     cd nix-*
-    # TODO is this still needed after reverting to 2.0.4?
-    # TODO if so, find from script path
-    # patch src/libstore/local-store.cc /global/home/users/jefdaj/nix-no-root/local-store.patch
+    # workaround for a bug when nix store is on an nfs filesystem
+    # see https://github.com/NixOS/nixpkgs/issues/29778
+    # TODO find from script path
+    patch src/libstore/local-store.cc /global/home/users/jefdaj/nix-no-root/local-store.patch
     ./configure --prefix=$BOOTSTRAP_DIR --with-store-dir=$NIX_DIR/store --localstatedir=$NIX_DIR/var
-    make
+    LDFLAGS=-static make
     make install
   fi
 }
@@ -72,6 +74,14 @@ nix_build_nix() {
   export LDFLAGS="-L$BOOTSTRAP_DIR/lib $LDFLAGS"
   export CPPFLAGS="-I$BOOTSTRAP_DIR/include $CPPFLAGS"
   $BOOTSTRAP_DIR/bin/nix-env -i nix $@
+  # module load boost/1.66.0
+  # module load gcc/6.3.0
+  # ldp=$LD_LIBRARY_PATH
+  # lp=$LIBRARY_PATH
+  # unset LD_LIBRARY_PATH
+  # unset LIBRARY_PATH
+  # export LIBRARY_PATH=$lp LD_LIBRARY_PATH=$ldp
+  # LD_LIBRARY_PATH= LIBRARY_PATH= LDFLAGS= CPPFLAGS= PKG_CONFIG_PATH= $BOOTSTRAP_DIR/bin/nix-env -i nix $@
 }
 
 main() {
